7aea55a72641b6df5876927e1292da49f4276170
sundi <didee@didee.cn>
2017-08-14 17:22:39 +0800

    Initial Commit

---
+++ ASDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASDKDemo
+//
+//  Created by Dee on 2017/8/14.
+//  Copyright © 2017年 Dee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASDKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASDKDemo
+//
+//  Created by Dee on 2017/8/14.
+//  Copyright © 2017年 Dee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
